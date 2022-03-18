7ffb46e770b7394280318ed0e27703808e25af73
interesnij <ochkarik1983@mail.ru>
2022-03-18 13:07:28 +0300

    J

--- src/views/blog_progs.rs
+++ src/views/blog_progs.rs
@@ -385,7 +385,8 @@ pub async fn edit_content_blog(mut payload: Multipart, _id: web::Path<i32>) -> i
     let _new_content = Some(form.content.clone());
     diesel::update(&_blog[0])
     .set(schema::blogs::content.eq(&_new_content))
-    .get_result::<Blog>(&_connection);
+    .get_result::<Blog>(&_connection)
+    .expect("Error.");
 
     HttpResponse::Ok()
 }
