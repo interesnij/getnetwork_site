e7a328218de1f2f1934644d127f4443952c78312
interesnij <ochkarik1983@mail.ru>
2022-03-18 09:47:22 +0300

    J

--- src/utils/payload_handler.rs
+++ src/utils/payload_handler.rs
@@ -69,7 +69,7 @@ pub async fn split_payload(payload: &mut Multipart) -> Forms {
                 if let Ok(s) = str::from_utf8(&data) {
                     let data_string = s.to_string();
                     if field.name() == "title" {
-                        form.title = data_string;
+                        form.title = &data_string;
                     } else if field.name() == "content" {
                         _content += data_string;
                     } else if field.name() == "description" {
