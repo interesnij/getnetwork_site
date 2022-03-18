258b2683b5817244248d9201cb348b0d910f7a82
interesnij <ochkarik1983@mail.ru>
2022-03-18 15:24:50 +0300

    J

--- src/views/blog_progs.rs
+++ src/views/blog_progs.rs
@@ -149,6 +149,10 @@ pub async fn edit_blog_page(req: HttpRequest, tera: web::Data<Tera>, _id: web::P
     HttpResponse::Ok().body(_rendered)
 }
 
+#[derive(Debug, Deserialize)]
+pub struct Params {
+    content: String,
+}
 pub async fn edit_content_blog_page(req: HttpRequest, tera: web::Data<Tera>, _id: web::Path<i32>) -> impl Responder {
     use schema::blogs::dsl::*;
 
@@ -164,7 +168,8 @@ pub async fn edit_content_blog_page(req: HttpRequest, tera: web::Data<Tera>, _id
     let _connection = establish_connection();
     let _blog = blogs.filter(schema::blogs::id.eq(&_blog_id)).load::<Blog>(&_connection).expect("E");
 
-    let _new_content = req.query_string().get("content").unwrap();
+    let params = web::Query::<Params>::from_query(req.query_string()).unwrap();
+    let _new_content = params.content;
     if _new_content != "" {
     diesel::update(&_blog[0])
         .set(schema::blogs::content.eq(&_new_content))
