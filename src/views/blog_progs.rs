0080b68626f31ebf65e9612aefcc2b9c7d754c16
interesnij <ochkarik1983@mail.ru>
2022-03-18 14:45:53 +0300

    J

--- src/views/blog_progs.rs
+++ src/views/blog_progs.rs
@@ -398,7 +398,7 @@ pub async fn edit_blog(mut payload: Multipart, _id: web::Path<i32>) -> impl Resp
 use serde::{Serialize, Deserialize};
 #[derive(Serialize, Deserialize)]
 pub struct BlogContent {
-    pub content: String,
+    pub _content: String,
 }
 pub async fn edit_content_blog(form: web::Form<BlogContent>, _id: web::Path<i32>) -> impl Responder {
     use crate::schema::blogs::dsl::blogs;
@@ -407,7 +407,7 @@ pub async fn edit_content_blog(form: web::Form<BlogContent>, _id: web::Path<i32>
     let _blog_id : i32 = *_id;
     let _blog = blogs.filter(schema::blogs::id.eq(_blog_id)).load::<Blog>(&_connection).expect("E");
 
-    let _new_content : String = form.content.clone();
+    let _new_content : String = form._content.clone();
     diesel::update(&_blog[0])
         .set(schema::blogs::content.eq(&_new_content))
         .get_result::<Blog>(&_connection)
