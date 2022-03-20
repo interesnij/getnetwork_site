5aac2b901c391195c86b0b01aa91f7d9bfd8df7e
interesnij <ochkarik1983@mail.ru>
2022-03-20 17:31:39 +0300

    J

--- src/views/service_progs.rs
+++ src/views/service_progs.rs
@@ -129,7 +129,7 @@ pub async fn create_service_page(req: HttpRequest, tera: web::Data<Tera>) -> imp
         for __cat in __serve_categories.iter() {
             _serve_count += 1;
             let mut _serve_int : String = _serve_count.to_string().parse().unwrap();
-            let _let_serves: String = _serve_int + "serves".to_string() + &_serve_int;
+            let _let_serves: String = _serve_int + "serves".to_string() + _serve_int;
             let __serves :Vec<Serve> = serve.filter(schema::serve::serve_categories.eq(__cat.id)).load(&_connection).expect("E.");
             data.insert(&_let_serves, &__serves);
         }
