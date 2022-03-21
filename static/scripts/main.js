52bb2976de7c391c1286b8b1378c75e8ebd94452
interesnij <ochkarik1983@mail.ru>
2022-03-21 22:01:25 +0300

    J

--- static/scripts/main.js
+++ static/scripts/main.js
@@ -119,7 +119,7 @@ function service_tab_action(is, tab_class){
 function on(elSelector, eventName, selector, fn) {var element = document.querySelector(elSelector);element.addEventListener(eventName, function(event) {var possibleTargets = element.querySelectorAll(selector);var target = event.target;for (var i = 0, l = possibleTargets.length; i < l; i++) {var el = target;var p = possibleTargets[i];while (el && el !== element) {if (el === p) {return fn.call(p, event);}el = el.parentNode;}}});};
 
 function get_custom_design() {
-  color == "white";
+  color = "white";
   params = window.location.search.replace( '?', '').split('&');
     if (params[0] && params[0].split("=")[0] == "f") {
       color = params[0].split("=")[1] }
