e2a9d0c35e96f2eb0e32227468543cfd05a4e0bb
interesnij <ochkarik1983@mail.ru>
2022-03-26 10:37:44 +0300

    J

--- src/views/pages.rs
+++ src/views/pages.rs
@@ -292,7 +292,7 @@ pub async fn search_page(req: HttpRequest, tera: web::Data<Tera>) -> impl Respon
     let _connection = establish_connection();
     let params = web::Query::<SearchParams>::from_query(&req.query_string()).unwrap();
     let _q = params.q.clone();
-    let _q_standalone = "%".to_owned() + _q + "%";
+    let _q_standalone = "%".to_owned() + &_q + "%";
 
     let _blogs = schema::blogs::table
         .filter(schema::blogs::title.eq(&_q))
