d4ef58ac77860a54ee9a185400de5d09b2d941e7
interesnij <ochkarik1983@mail.ru>
2022-03-30 18:25:34 +0300

    m

--- static/scripts/main.js
+++ static/scripts/main.js
@@ -410,12 +410,17 @@ on('body', 'click', '.prev_item', function(event) {
 
 on('body', 'input', '.general_search', function() {
     _this = this;
-    if (this.classList.contains("search-field") && !document.body.querySelector(".search_section")) {
-      ajax_get_reload("/search/?q=" + this.value)
+    if (_this.classList.contains("search-field") && !document.body.querySelector(".search_section")) {
+      ajax_get_reload("/search/?q=" + _this.value)
     }
     else if (document.body.querySelector(".search_section")) {
+      if (_this.getAttribute("data-folder")) {
+        folder = _this.getAttribute("data-folder")
+      } else {
+        folder = ""
+      };
     var ajax_link = window.XMLHttpRequest ? new XMLHttpRequest() : new ActiveXObject( 'Microsoft.XMLHTTP' );
-      ajax_link.open( 'GET', "/search/" + this.getAttribute("data-folder") + "?q=" + this.value, true );
+      ajax_link.open( 'GET', "/search/" + _this.getAttribute("data-folder") + "?q=" + _this.value, true );
       ajax_link.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
       ajax_link.onreadystatechange = function () {
         if ( this.readyState == 4 && this.status == 200 ) {
