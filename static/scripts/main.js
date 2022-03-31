23272f18f7961012886273510447e7272ffc5661
interesnij <ochkarik1983@mail.ru>
2022-03-31 11:44:43 +0300

    J

--- static/scripts/main.js
+++ static/scripts/main.js
@@ -356,7 +356,7 @@ get_custom_design();
 
 on('body', 'click', '.ajax', function(event) {
   event.preventDefault();
-  url = this.getAttribute("href");
+  url = location.host + this.getAttribute("href");
   _href = window.location.href;
   _search = window.location.search;
   _params = _search.replace( '?', '').split('&');
