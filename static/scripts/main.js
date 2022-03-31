b19246d25b77c5cfc4c92bad1cb81c1cc6786281
interesnij <ochkarik1983@mail.ru>
2022-03-31 17:55:55 +0300

    J

--- static/scripts/main.js
+++ static/scripts/main.js
@@ -325,13 +325,13 @@ function service_tab_action(is, tab_class){
   if (!is.classList.contains("active")){
   nav = is.parentElement.parentElement.parentElement;
   nav_items = document.body.querySelectorAll(".yy");
-  for (var i = 0; i < nav_items.length; i++){nav_items[i].classList.remove("active"); nav_items[i].classList.add("pointer")};
-  is.classList.add("active"); is.classList.remove("pointer");
+  for (var i = 0; i < nav_items.length; i++){nav_items[i].classList.remove("active", "in"); nav_items[i].classList.add("pointer")};
+  is.classList.add("active", "in"); is.classList.remove("pointer");
   tabs = document.body.querySelector(".tab-content");
   tabs_items = tabs.querySelectorAll(".tab-pane");
-  for (var i = 0; i < tabs_items.length; i++){tabs_items[i].classList.remove("active")};
+  for (var i = 0; i < tabs_items.length; i++){tabs_items[i].classList.remove("active", "in")};
   cur = tabs.querySelector(tab_class);
-  cur.classList.add("active")
+  cur.classList.add("active", "in")
 }};
 
 function on(elSelector, eventName, selector, fn) {var element = document.querySelector(elSelector);element.addEventListener(eventName, function(event) {var possibleTargets = element.querySelectorAll(selector);var target = event.target;for (var i = 0, l = possibleTargets.length; i < l; i++) {var el = target;var p = possibleTargets[i];while (el && el !== element) {if (el === p) {return fn.call(p, event);}el = el.parentNode;}}});};
@@ -358,7 +358,7 @@ on('body', 'click', '.ajax', function(event) {
   event.preventDefault();
   if (this.getAttribute("href") == window.location.pathname){
     toast_info("Вы уже на этой странице");
-    return  
+    return
   };
 
   url = "http://" + location.host + this.getAttribute("href");
