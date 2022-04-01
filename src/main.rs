e08bdb0d6168f3ac127b2c5efc952a7a066752dd
interesnij <ochkarik1983@mail.ru>
2022-04-01 20:50:29 +0300

    J

--- src/main.rs
+++ src/main.rs
@@ -35,7 +35,7 @@ async fn main() -> std::io::Result<()> {
             .service(_files2)
             .configure(routes)
     })
-    .bind("151.248.120.138:8013")?
+    .bind("151.248.120.138:8014")?
     //.bind("http://вселенная.рус:8000")?
     .run()
     .await
