76844f95a863f5ba2fff46657e975f363175269a
interesnij <ochkarik1983@mail.ru>
2022-04-02 20:55:59 +0300

    J

--- src/views/service_progs.rs
+++ src/views/service_progs.rs
@@ -395,7 +395,7 @@ pub async fn get_service_page(req: HttpRequest, tera: web::Data<Tera>, param: we
                 let _serve_list = serve
                     .filter(schema::serve::serve_categories.eq(__cat.id))
                     .filter(schema::serve::id.eq(any(&serve_ids)))
-                    .order(schema::serve::serve_position.asc())
+                    //.order(schema::serve::serve_position.asc())
                     .load::<Serve>(&_connection)
                     .expect("E.");
                 data.insert(&_let_serves, &_serve_list);
