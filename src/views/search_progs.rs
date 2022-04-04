a5a980a6a74c2d4750fa56f5fc3a843d486a5b70
interesnij <ochkarik1983@mail.ru>
2022-04-04 16:52:29 +0300

    J

--- src/views/search_progs.rs
+++ src/views/search_progs.rs
@@ -22,7 +22,7 @@ pub async fn search_page(req: HttpRequest, tera: web::Data<Tera>) -> impl Respon
     let _connection = establish_connection();
 
     let params = Ok(web::Query::<SearchParams>::from_query(&req.query_string()).unwrap());
-
+    println("{:?}", params);
     let _q = params.q.clone();
     let _q_standalone = "%".to_owned() + &_q + "%";
 
