49cdd452565d01cc0ef95fe02854b9c5c9ccec03
interesnij <ochkarik1983@mail.ru>
2022-04-04 18:32:27 +0300

    J

--- src/views/pages.rs
+++ src/views/pages.rs
@@ -11,6 +11,10 @@ use crate::NewUser;
 use crate::diesel::RunQueryDsl;
 
 
+#[derive(Debug, Deserialize)]
+pub struct SParams {
+    pub q: String,
+}
 pub async fn index(req: HttpRequest, tera: web::Data<Tera>) -> impl Responder {
     use crate::diesel::QueryDsl;
     use crate::diesel::ExpressionMethods;
@@ -30,6 +34,8 @@ pub async fn index(req: HttpRequest, tera: web::Data<Tera>) -> impl Responder {
     let _last_stores :Vec<Store> = stores.filter(is_store_active.eq(true)).order(store_created.desc()).limit(3).load(&_connection).expect(".");
 
     let mut data = Context::new();
+    let params = web::Query::<SParams>::from_query(&req.query_string())
+    println!("{:?}", params);
     let (_type, _is_admin, _service_cats, _store_cats, _blog_cats, _wiki_cats, _work_cats) = get_template_2(req);
     data.insert("service_categories", &_service_cats);
     data.insert("store_categories", &_store_cats);
