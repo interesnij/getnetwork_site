3bb624e478d7c0950f8a0b353586fedc9941ff35
interesnij <ochkarik1983@mail.ru>
2022-07-22 16:59:25 +0300

    J

--- src/models/tag.rs
+++ src/models/tag.rs
@@ -30,26 +30,26 @@ pub struct Tag {
     pub user_id:       i32,
 }
 impl Tag {
-    pub fn get_tags_list(&self, page: i32, limit: i32) -> (Vec<Tag>, i32) {
+    pub fn get_tags_list(page: i32, limit: i32) -> (Vec<Tag>, i32) {
         let mut next_page_number = 0;
         let have_next: i32;
         let object_list: Vec<Tag>;
 
         if page > 1 {
             have_next = page * limit + 1;
-            object_list = self.get_tags(limit.into(), have_next.into());
+            object_list = Tag::get_tags(limit.into(), have_next.into());
         }
         else {
             have_next = limit + 1;
-            object_list = self.get_tags(limit.into(), 0);
+            object_list = Tag::get_tags(limit.into(), 0);
         }
-        if self.get_tags(1, have_next.into()).len() > 0 {
+        if Tag::get_tags(1, have_next.into()).len() > 0 {
             next_page_number = page + 1;
         }
 
         return (object_list, next_page_number);
     }
-    pub fn get_tags(&self, limit: i64, offset: i64) -> Vec<Tag> {
+    pub fn get_tags(limit: i64, offset: i64) -> Vec<Tag> {
         use crate::schema::tags::dsl::tags;
 
         let _connection = establish_connection();
