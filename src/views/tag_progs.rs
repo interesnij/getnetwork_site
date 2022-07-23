c1e1cf424d1c65dcceafe9bf1ab2b2135e525fa1
interesnij <ochkarik1983@mail.ru>
2022-07-23 11:54:46 +0300

    J

--- src/views/tag_progs.rs
+++ src/views/tag_progs.rs
@@ -129,7 +129,7 @@ pub async fn create_tag(session: Session, mut payload: Multipart) -> impl Respon
 }
 
 pub async fn tag_page(req: HttpRequest, session: Session, _id: web::Path<i32>) -> actix_web::Result<HttpResponse> {
-    use schema::tags::dsl::tags;
+    use schema::{tags::dsl::tags, tags_items::dsl::tags_items,};
     use crate::utils::{get_request_user_data, get_device_and_ajax};
     use crate::models::{Work, Blog, Service, Store, Wiki};
 
