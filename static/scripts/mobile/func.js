336c04117b8417b691e031cb37f0b5eb819fe1ea
interesnij <ochkarik1983@mail.ru>
2022-08-01 09:38:16 +0300

    J

--- static/scripts/mobile/func.js
+++ static/scripts/mobile/func.js
@@ -1,5 +1,5 @@
 $mobile_nav = document.body.querySelector(".mobile_nav");
-
+console.log($mobile_nav);
 function on(elSelector, eventName, selector, fn) {var element = document.querySelector(elSelector);element.addEventListener(eventName, function(event) {var possibleTargets = element.querySelectorAll(selector);var target = event.target;for (var i = 0, l = possibleTargets.length; i < l; i++) {var el = target;var p = possibleTargets[i];while (el && el !== element) {if (el === p) {return fn.call(p, event);}el = el.parentNode;}}});};
 
 function mob_menu_hide() {
