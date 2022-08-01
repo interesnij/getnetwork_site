9a7af5752f8e1dc0c0a472d9fb01e6118a344336
interesnij <ochkarik1983@mail.ru>
2022-08-01 09:32:58 +0300

    J

--- static/scripts/mobile/func.js
+++ static/scripts/mobile/func.js
@@ -1,4 +1,5 @@
 $mobile_nav = document.body.querySelector(".mobile_nav");
+
 function on(elSelector, eventName, selector, fn) {var element = document.querySelector(elSelector);element.addEventListener(eventName, function(event) {var possibleTargets = element.querySelectorAll(selector);var target = event.target;for (var i = 0, l = possibleTargets.length; i < l; i++) {var el = target;var p = possibleTargets[i];while (el && el !== element) {if (el === p) {return fn.call(p, event);}el = el.parentNode;}}});};
 
 function mob_menu_hide() {
@@ -67,80 +68,90 @@ function show_nav_first_span(){
   $mobile_nav.querySelector(".pages_btn").classList.remove("mobile_icon_active")
 };
 function hide_nav_first_span(){
-  first_span = $mobile_nav.previousElementSibling.previousElementSibling;
-  first_span.style.display = "none"; first_span.classList.remove("btn_active");
-  deactivate_nav_buttons();
-  $mobile_nav.querySelector(".apps_btn").classList.remove("mobile_icon_active");
+  try {
+    first_span = $mobile_nav.previousElementSibling.previousElementSibling;
+    first_span.style.display = "none"; first_span.classList.remove("btn_active");
+    deactivate_nav_buttons();
+    $mobile_nav.querySelector(".apps_btn").classList.remove("mobile_icon_active");
+  } catch { null }
 };
 function toggle_nav_first_span(){
-  first_span = $mobile_nav.previousElementSibling.previousElementSibling;
-  first_span.classList.contains("btn_active") ? (hide_nav_first_span(), first_span.classList.remove("btn_active")) : (show_nav_first_span(), first_span.classList.add("btn_active"))
+  try {
+    first_span = $mobile_nav.previousElementSibling.previousElementSibling;
+    first_span.classList.contains("btn_active") ? (hide_nav_first_span(), first_span.classList.remove("btn_active")) : (show_nav_first_span(), first_span.classList.add("btn_active"))
+  } catch { null }
 };
 
 function show_nav_second_span(){
-  second_span = $mobile_nav.previousElementSibling;
-  second_span.style.display = "flex"; second_span.classList.add("btn_active");
-  hide_nav_first_span();
-  $mobile_nav.querySelector(".apps_btn").classList.remove("mobile_icon_active");
-  $mobile_nav.querySelector(".pages_btn").classList.add("mobile_icon_active")
+  try {
+    second_span = $mobile_nav.previousElementSibling;
+    second_span.style.display = "flex"; second_span.classList.add("btn_active");
+    hide_nav_first_span();
+    $mobile_nav.querySelector(".apps_btn").classList.remove("mobile_icon_active");
+    $mobile_nav.querySelector(".pages_btn").classList.add("mobile_icon_active")
+  } catch { null }
 };
 function hide_nav_second_span(){
-  second_span = $mobile_nav.previousElementSibling;
-  second_span.style.display = "none"; second_span.classList.remove("btn_active");
-  deactivate_nav_buttons();
-  $mobile_nav.querySelector(".pages_btn").classList.remove("mobile_icon_active");
+  try {
+    second_span = $mobile_nav.previousElementSibling;
+    second_span.style.display = "none"; second_span.classList.remove("btn_active");
+    deactivate_nav_buttons();
+    $mobile_nav.querySelector(".pages_btn").classList.remove("mobile_icon_active");
+  } catch { null }
 };
 function toggle_nav_second_span(){
-  second_span = $mobile_nav.previousElementSibling;
-  second_span.classList.contains("btn_active") ? (hide_nav_second_span(), second_span.classList.remove("btn_active")) : (show_nav_second_span(), second_span.classList.add("btn_active"))
+  try {
+    second_span = $mobile_nav.previousElementSibling;
+    second_span.classList.contains("btn_active") ? (hide_nav_second_span(), second_span.classList.remove("btn_active")) : (show_nav_second_span(), second_span.classList.add("btn_active"))
+  } catch { null }
 };
 
 function get_active_button() {
-  buttons = $mobile_nav.parentElement.querySelectorAll(".mobile_icon");
-  path = document.location.pathname;
-  for (var i = 0; i < buttons.length; i++){buttons[i].classList.remove("mobile_icon_current")};
-  if (path == "/") {
-      buttons[10].classList.add("mobile_icon_current");
+  try {
+    buttons = $mobile_nav.parentElement.querySelectorAll(".mobile_icon");
+    path = document.location.pathname;
+    for (var i = 0; i < buttons.length; i++){buttons[i].classList.remove("mobile_icon_current")};
+    if (path == "/") {
+        buttons[10].classList.add("mobile_icon_current");
     }
-  else if (path.includes('service')) {
-    buttons[0].classList.add("mobile_icon_current");
-    buttons[11].classList.add("mobile_icon_current");
-  }
-  else if (path.includes('work')) {
-    buttons[1].classList.add("mobile_icon_current");
-    buttons[11].classList.add("mobile_icon_current");
-
-  }
-  else if (path.includes('store')) {
-    buttons[2].classList.add("mobile_icon_current");
-    buttons[11].classList.add("mobile_icon_current");
-  }
-  else if (path.includes('blog')) {
-    buttons[3].classList.add("mobile_icon_current");
-    buttons[11].classList.add("mobile_icon_current");
-  }
-  else if (path.includes('wiki')) {
-    buttons[4].classList.add("mobile_icon_current");
-    buttons[11].classList.add("mobile_icon_current");
-  }
-
-  else if (path == "/contacts/") {
-    buttons[5].classList.add("mobile_icon_current");
-    buttons[12].classList.add("mobile_icon_current");
-  }
-  else if (path == "/about/") {
-    buttons[6].classList.add("mobile_icon_current");
-    buttons[12].classList.add("mobile_icon_current");
-  }
-  else if (path == "/tags/") {
-    buttons[7].classList.add("mobile_icon_current");
-    buttons[12].classList.add("mobile_icon_current");
-  }
-  else if (path == "/search/") {
-    buttons[8].classList.add("mobile_icon_current");
-    buttons[12].classList.add("mobile_icon_current");
-  }
-  else if (path == "/auth/" || path.substr(1, 5) == "users") {
-    buttons[13].classList.add("mobile_icon_current");
+    else if (path.includes('service')) {
+      buttons[0].classList.add("mobile_icon_current");
+      buttons[11].classList.add("mobile_icon_current");
     }
+    else if (path.includes('work')) {
+      buttons[1].classList.add("mobile_icon_current");
+      buttons[11].classList.add("mobile_icon_current");
+    }
+    else if (path.includes('store')) {
+      buttons[2].classList.add("mobile_icon_current");
+      buttons[11].classList.add("mobile_icon_current");
+    }
+    else if (path.includes('blog')) {
+      buttons[3].classList.add("mobile_icon_current");
+      buttons[11].classList.add("mobile_icon_current");
+    }
+    else if (path.includes('wiki')) {
+      buttons[4].classList.add("mobile_icon_current");
+      buttons[11].classList.add("mobile_icon_current");
+    }
+    else if (path == "/contacts/") {
+      buttons[5].classList.add("mobile_icon_current");
+      buttons[12].classList.add("mobile_icon_current");
+    }
+    else if (path == "/about/") {
+      buttons[6].classList.add("mobile_icon_current");
+      buttons[12].classList.add("mobile_icon_current");
+    }
+    else if (path == "/tags/") {
+      buttons[7].classList.add("mobile_icon_current");
+      buttons[12].classList.add("mobile_icon_current");
+    }
+    else if (path == "/search/") {
+      buttons[8].classList.add("mobile_icon_current");
+      buttons[12].classList.add("mobile_icon_current");
+    }
+    else if (path == "/auth/" || path.substr(1, 5) == "users") {
+      buttons[13].classList.add("mobile_icon_current");
+    }
+  } catch { null }
 };
