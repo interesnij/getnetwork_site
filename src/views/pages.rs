24dd50e6786ea6f4385b0a92fbbbfaa886f2ad88
interesnij <ochkarik1983@mail.ru>
2022-08-03 12:35:47 +0300

    J

--- src/views/pages.rs
+++ src/views/pages.rs
@@ -44,6 +44,38 @@ pub async fn index(req: HttpRequest, session: Session) -> actix_web::Result<Http
     // первая отрисовка страницы - организуем скрытие информации
     if is_ajax == 0 {
         get_first_load_page(&session, is_desctop, "Главная страница".to_string()).await
+        println!(
+            "User's Name            whoami::realname():    {}",
+            whoami::realname()
+        );
+        println!(
+            "User's Username        whoami::username():    {}",
+            whoami::username()
+        );
+        println!(
+            "User's Language        whoami::lang():        {:?}",
+            whoami::lang().collect::<Vec<String>>()
+        );
+        println!(
+            "Device's Pretty Name   whoami::devicename():  {}",
+            whoami::devicename()
+        );
+        println!(
+            "Device's Hostname      whoami::hostname():    {}",
+            whoami::hostname()
+        );
+        println!(
+            "Device's Platform      whoami::platform():    {}",
+            whoami::platform()
+        );
+        println!(
+            "Device's OS Distro     whoami::distro():      {}",
+            whoami::distro()
+        );
+        println!(
+            "Device's Desktop Env.  whoami::desktop_env(): {}",
+            whoami::desktop_env()
+        );
     }
     else {
         use crate::models::{Work, Service, Wiki, Blog, Store};
