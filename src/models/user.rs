e66208b1dd4369d3eef5e18245bc0ba8d6405c3e
interesnij <ochkarik1983@mail.ru>
2022-08-07 17:03:29 +0300

    J

--- src/models/user.rs
+++ src/models/user.rs
@@ -199,13 +199,6 @@ pub struct HistoryResponse {
     pub height:  f64,
     pub seconds: i32,
 }
-#[derive(Debug, Queryable, Serialize)]
-pub struct CookieStatCard {
-    pub link:     String,
-    pub title:    String,
-    pub height:   f64,
-    pub seconds:  i32,
-}
 
 impl CookieStat {
     pub fn get_stat_list(user_id: i32, page: i32, limit: i32) -> (Vec<CookieStat>, i32) {
@@ -216,13 +209,13 @@ impl CookieStat {
         if page > 1 {
             let step = (page - 1) * 20;
             have_next = page * limit + 1;
-            object_list = self.get_stat_items(user_id, limit.into(), step.into());
+            object_list = CookieStat::get_stat_items(user_id, limit.into(), step.into());
         }
         else {
             have_next = limit + 1;
-            object_list = self.get_stat_items(user_id, limit.into(), 0);
+            object_list = CookieStat::get_stat_items(user_id, limit.into(), 0);
         }
-        if self.get_stat_items(user_id, 1, have_next.into()).len() > 0 {
+        if CookieStat::get_stat_items(user_id, 1, have_next.into()).len() > 0 {
             next_page_number = page + 1;
         }
 
