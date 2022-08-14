96efa7b0e466c9874cd6561efebf930f3f73a291
interesnij <ochkarik1983@mail.ru>
2022-08-14 18:12:35 +0300

    J

--- static/scripts/main.js
+++ static/scripts/main.js
@@ -652,9 +652,9 @@ on('body', 'click', '.anon_color_change', function() {
   }
   if (color == "white") {
     addStyleSheets("/static/styles/color/daark.css");
-    this.setAttribute("data-color", "daark");
-    new_color = "daark"
-  } else if (color == "daark") {
+    this.setAttribute("data-color", "dark");
+    new_color = "dark"
+  } else if (color == "dark") {
     addStyleSheets("/static/styles/color/yellow.css");
     this.setAttribute("data-color", "yellow");
     new_color = "yellow"
