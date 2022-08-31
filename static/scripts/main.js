9dbc90ff9d34d07eb7beb6e317c4bc1c80d9c3a5
interesnij <ochkarik1983@mail.ru>
2022-08-31 17:53:02 +0300

    J

--- static/scripts/main.js
+++ static/scripts/main.js
@@ -362,28 +362,28 @@ function create_order_form() {
       cat_id = banner_blocks[i].querySelector(".get_serve_category_info").getAttribute("data-pk");
 
       tabs = banner_blocks[i].querySelector(".price_mode");
-      tab_pk = tabs.querySelector(".active");
+      tab_pk = tabs.querySelector(".active").getAttribute("data-pk");
       tab_panes = banner_blocks[i].querySelectorAll(".tab-pane");
       for (var k = 0; k < tab_panes.length; k++) {
         if (tab_panes[k].getAttribute("data-pk") == tab_pk) {
-          current_tab = tab_panes[k];
+          serves_list = tab_panes[k].querySelectorAll(".hover");
+          serves = "";
+          for (var i = 0; i < serves_list.length; i++) {
+            serve = "<tr><td style='width:55%'><a class='get_serve_info pointer' data-pk='"
+            + serves_list[i].querySelector(".get_serve_info").getAttribute("data-pk")
+            + "'>"
+            + serves_list[i].querySelector(".get_serve_info").innerHTML
+            + "</a></td><td class='price_td' style='width:15%'>"
+            + serves_list[i].querySelector(".price_td").innerHTML
+            + "</td><td class='hours' style='width:15%'>"
+            + serves_list[i].querySelector(".hours").innerHTML
+            + "</td></tr>";
+
+            serves += serve;
+          }
         }
       }
-      serves_list = current_tab.querySelectorAll(".hover");
-      serves = "";
-      for (var i = 0; i < serves_list.length; i++) {
-        serve = "<tr><td style='width:55%'><a class='get_serve_info pointer' data-pk='"
-        + serves_list[i].querySelector(".get_serve_info").getAttribute("data-pk")
-        + "'>"
-        + serves_list[i].querySelector(".get_serve_info").innerHTML
-        + "</a></td><td class='price_td' style='width:15%'>"
-        + serves_list[i].querySelector(".price_td").innerHTML
-        + "</td><td class='hours' style='width:15%'>"
-        + serves_list[i].querySelector(".hours").innerHTML
-        + "</td></tr>";
 
-        serves += serve;
-      }
 
       section = "<section class='banner_block mb-20 mt-20 border' style='width:100%;'><h4 class='section-title font-alt'>"
       + banner_block_title
