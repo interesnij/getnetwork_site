9be99f264a5efeeb86d1d55a722cd9c4af4c2beb
interesnij <ochkarik1983@mail.ru>
2022-09-05 09:20:41 +0300

    J

--- src/main.rs
+++ src/main.rs
@@ -30,8 +30,8 @@ async fn main() -> std::io::Result<()> {
         let _files = Files::new("/static", "static/").show_files_listing();
         let _files2 = Files::new("/media", "media/").show_files_listing();
         let cors = Cors::default()
-            .allowed_origin("http://вебсервисы.рф")
-            .allowed_origin("https://вебсервисы.рф")
+            //.allowed_origin("http://вебсервисы.рф")
+            //.allowed_origin("https://вебсервисы.рф")
             //.allowed_origin("194.58.90.123:8084")
             .allowed_methods(vec!["GET", "POST"])
             .max_age(3600);
