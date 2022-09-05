877d17f0548376de601c4f0c8b3c3fdb40d6b839
interesnij <ochkarik1983@mail.ru>
2022-09-05 09:44:49 +0300

    J

--- src/main.rs
+++ src/main.rs
@@ -32,7 +32,8 @@ async fn main() -> std::io::Result<()> {
         let cors = Cors::default()
             //.allowed_origin("http://вебсервисы.рф")
             //.allowed_origin("https://вебсервисы.рф")
-            //.allowed_origin("194.58.90.123:8084")
+            .allowed_origin("194.58.90.123:8084")
+            .allowed_origin("127.0.0.1:6379")
             .allowed_methods(vec!["GET", "POST"])
             .max_age(3600);
         App::new()
