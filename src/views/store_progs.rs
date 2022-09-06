08d71810ed8616c5de1c42a51ffe4e7d4412af0d
interesnij <ochkarik1983@mail.ru>
2022-09-06 16:40:08 +0300

    J

--- src/views/store_progs.rs
+++ src/views/store_progs.rs
@@ -1508,24 +1508,24 @@ pub async fn store_categories_page(session: Session, req: HttpRequest) -> actix_
     }
 }
 
-pub async fn publish_blog(session: Session, _id: web::Path<i32>) -> impl Responder {
+pub async fn publish_store(session: Session, _id: web::Path<i32>) -> impl Responder {
     if is_signed_in(&session) {
         let _request_user = get_request_user_data(&session);
         if _request_user.perm == 60 {
-            use crate::schema::blogs::dsl::blogs;
+            use crate::schema::stores::dsl::stores;
 
             let _connection = establish_connection();
             let _id: i32 = *_id;
-            let _blog = blogs
-                .filter(schema::blogs::id.eq(_id))
-                .load::<Blog>(&_connection)
+            let _store = stores
+                .filter(schema::stores::id.eq(_id))
+                .load::<Store>(&_connection)
                 .expect("E")
                 .into_iter()
                 .nth(0)
                 .unwrap();
-            diesel::update(&_blog)
-                .set(schema::blogs::is_active.eq(true))
-                .get_result::<Blog>(&_connection)
+            diesel::update(&_store)
+                .set(schema::stores::is_active.eq(true))
+                .get_result::<Store>(&_connection)
                 .expect("Error.");
         }
     }
