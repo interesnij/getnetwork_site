aef1f7b468f192f6eade20a29c0d4f43758e0051
interesnij <ochkarik1983@mail.ru>
2022-09-06 13:10:46 +0300

    J

--- static/scripts/main.js
+++ static/scripts/main.js
@@ -1130,6 +1130,6 @@ on('body', 'click', '#create_feedback_btn', function() {
 });
 
 window.addEventListener('popstate', function (e) {
-  console.log("lendth", window.history.lendth);
-  console.log("state", history.state)
+  console.log("lendth", window.history.length);
+  console.log("state", history)
 })
