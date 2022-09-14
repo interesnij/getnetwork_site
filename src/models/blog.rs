beb72f6f6de84282fe2f90fa717b2d4b55f89e97
interesnij <ochkarik1983@mail.ru>
2022-09-14 22:00:11 +0300

    m

--- src/models/blog.rs
+++ src/models/blog.rs
@@ -355,7 +355,7 @@ impl Blog {
         let _connection = establish_connection();
         return blog_images
             .filter(schema::blog_images::blog.eq(self.id))
-            .order(schema::blog_images::id.asc())
+            .order(schema::blog_images::id.desc())
             .select(schema::blog_images::id)
             .load::<i32>(&_connection)
             .expect("E");
