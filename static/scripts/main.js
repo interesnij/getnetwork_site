fa27aba62b50a99f26305fb45e0486b3433e5d2e
interesnij <ochkarik1983@mail.ru>
2022-09-29 09:41:24 +0300

    J

--- static/scripts/main.js
+++ static/scripts/main.js
@@ -222,7 +222,7 @@ function get_stat_meta() {
 
 window.unload = function() {
   navigator.sendBeacon("/create_history/", JSON.stringify(get_stat_meta()));
-});
+};
 
 ///////////////
 function get_or_create_cookie_user() {
