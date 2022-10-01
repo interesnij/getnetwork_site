114eee4dd2ba5ef7d4c40299b798d2ead5921a1c
interesnij <ochkarik1983@mail.ru>
2022-10-01 11:22:38 +0300

    J

--- src/views/pages.rs
+++ src/views/pages.rs
@@ -404,7 +404,7 @@ pub async fn history_page(conn: ConnectionInfo, req: HttpRequest, session: Sessi
             .unwrap();
 
         let page = get_page(&req);
-        let (object_list, next_page_number) = web::block(move || CookieStat::get_stat_list(user_id, page, 20)).await?;
+        let (object_list, next_page_number) = web::block(move || CookieStat::get_stat_list(user_id, page, 20)).await;
 
         if is_signed_in(&session) {
             let _request_user = get_request_user_data(&session);
@@ -832,7 +832,7 @@ pub async fn get_user_history_page(session: Session, req: HttpRequest, user_id: 
             use crate::utils::get_page;
             use crate::models::CookieStat;
 
-            let (object_list, next_page_number) = web::block(move || CookieStat::get_stat_list(*user_id, get_page(&req), 20)).await?;
+            let (object_list, next_page_number) = web::block(move || CookieStat::get_stat_list(*user_id, get_page(&req), 20)).await;
 
             #[derive(TemplateOnce)]
             #[template(path = "desctop/load/user_stat.stpl")]
