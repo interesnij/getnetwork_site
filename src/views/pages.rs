0c434b21df4af748aa5254935d00bba56e8f2507
interesnij <ochkarik1983@mail.ru>
2022-10-01 10:28:03 +0300

    J

--- src/views/pages.rs
+++ src/views/pages.rs
@@ -831,7 +831,8 @@ pub async fn get_user_history_page(session: Session, req: HttpRequest, user_id: 
             use crate::models::CookieStat;
 
             let res = web::block(move || CookieStat::get_stat_list(*user_id, get_page(&req), 20)).await?;
-            let (object_list, next_page_number) = Ok(res?);
+            let _res = res?;
+            let (object_list, next_page_number) = Ok(_res);
 
             #[derive(TemplateOnce)]
             #[template(path = "desctop/load/user_stat.stpl")]
