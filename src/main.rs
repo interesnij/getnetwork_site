e0728b0e9e156f55d68aac707f57d11a912855f5
interesnij <ochkarik1983@mail.ru>
2022-10-01 16:20:26 +0300

    J

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,12 @@ extern crate diesel;
 #[macro_use]
 extern crate log;
 
+use actix::Actor;
+use actix_cors::Cors;
+use actix_rt;
+use dotenv::dotenv;
+use env_logger;
+
 pub mod schema;
 pub mod models;
 pub mod routes;
@@ -13,8 +19,10 @@ mod vars;
 use actix_web::{
     HttpServer,
     App,
-    middleware,
+    middleware::{Compress, Logger},
+    http,
 };
+use actix_redis::RedisSession;
 use actix_files::Files;
 use crate::routes::routes;
 
@@ -26,25 +34,30 @@ mod views;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    use actix_redis::RedisSession;
-    //use actix_cors::Cors;
+    dotenv().ok();
+    env_logger::init_from_env(env_logger::Env::default().default_filter_or("debug"));
+    let server = websocket::Server::new().start();
 
     HttpServer::new(|| {
         let _files = Files::new("/static", "static/").show_files_listing();
         let _files2 = Files::new("/media", "media/").show_files_listing();
-        //let cors = Cors::permissive();
-            //Cors::default()
-            //.allowed_origin("127.0.0.1:6379")
-            //.allowed_origin("194.58.90.123")
-            //.allowed_origin("вебсервисы.рф")
-            //.allowed_origin("https://вебсервисы.рф")
+        let cors = Cors::default()
+            .allowed_origin(&env::var("CLIENT_HOST").unwrap())
+            .allow_any_method()
+            .allowed_headers(vec![
+                http::header::AUTHORIZATION,
+                http::header::ACCEPT,
+                http::header::CONTENT_TYPE,
+            ])
+            .max_age(3600);
 
-            //.allowed_methods(vec!["GET", "POST"])
-            //.max_age(3600);
         App::new()
-            //.wrap(cors)
-            .wrap(middleware::Compress::default())
+            .wrap(cors)
+            .wrap(Logger::default())
+            .wrap(Logger::new("%a %{User-Agent}i"))
+            .wrap(Compress::default())
             .wrap(RedisSession::new("127.0.0.1:6379", &[0; 32]))
+            .data(server.clone())
             .service(_files)
             .service(_files2)
             .configure(routes)
