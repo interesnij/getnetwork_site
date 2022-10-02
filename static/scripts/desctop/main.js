b95df2aeb07e3d74e85e8a26eb4e99b44cdaa6eb
interesnij <ochkarik1983@mail.ru>
2022-10-02 16:19:50 +0300

    J

--- static/scripts/desctop/main.js
+++ static/scripts/desctop/main.js
@@ -262,7 +262,7 @@ function connect() {
   const { location } = window
 
   const proto = location.protocol.startsWith('https') ? 'wss' : 'ws'
-  const wsUri = `${proto}://194.58.90.123:8082/ws`
+  const wsUri = `${proto}://127.0.0.1:8443/ws`
   //const wsUri = `${proto}://${location.host}/ws`
 
   console.log('Connecting...')
