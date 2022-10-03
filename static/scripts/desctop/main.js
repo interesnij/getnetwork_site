fface9140415196528effdbaa82b6fb58ada9b97
interesnij <ochkarik1983@mail.ru>
2022-10-03 07:32:07 +0300

    J

--- static/scripts/desctop/main.js
+++ static/scripts/desctop/main.js
@@ -263,9 +263,7 @@ function connect() {
 
   ws_scheme = window.location.protocol == "https:" ? "wss" : "ws";
   wsUri = ws_scheme + '://' + window.location.host + ":8443/ws";
-  wsUri = ws_scheme + "://194.58.90.123:8082/ws";
-  //const proto = location.protocol.startsWith('https') ? 'wss' : 'ws'
-  //const wsUri = proto + "://" + 194.58.90.123 + "/ws"
+  //wsUri = ws_scheme + "://194.58.90.123:8082/ws";
 
   console.log('Connecting...')
   socket = new WebSocket(wsUri)
