848238bfd573898b98d810bfb4c90f09e73b0bae
interesnij <ochkarik1983@mail.ru>
2022-10-03 08:54:43 +0300

    J

--- static/scripts/desctop/main.js
+++ static/scripts/desctop/main.js
@@ -274,6 +274,8 @@ function connect() {
 
   socket.onmessage = (ev) => {
     data = ev.data;
+    console.log(data.msg_type);
+    console.log(data.data);
     if (data.msg_type == "new_viewer" && document.body.querySelector(".real_wiew")) {
       real_wiew = document.body.querySelector(".real_wiew");
       counter = real_wiew.innerHTML*1;
