f28ee2b53c29250dbe05d193d9d4ad0618439aa8
interesnij <ochkarik1983@mail.ru>
2022-10-07 17:13:48 +0300

    J

--- src/views/work_progs.rs
+++ src/views/work_progs.rs
@@ -87,7 +87,8 @@ pub async fn get_work_page(session: Session, req: HttpRequest, param: web::Path<
             Err(_error) => Vec::new(),
         };
         let title = _item.title;
-        let tags_res = block(move || _item.get_tags()).await?;
+        let item_tags = _item.get_tags().await?;
+        let tags_res = block(move || item_tags);
         _tags = match tags_res {
             Ok(_list) => _list,
             Err(_error) => Vec::new(),
