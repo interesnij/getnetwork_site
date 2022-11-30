d21a866216ebaf375547e07e72ef12a6b071bd41
interesnij <ochkarik1983@mail.ru>
2022-11-30 12:11:12 +0300

    J

--- src/main.rs
+++ src/main.rs
@@ -49,9 +49,9 @@ async fn main() -> std::io::Result<()> {
         let _files2 = Files::new("/media", "media/").show_files_listing();
         let messages = Arc::new(Mutex::new(vec![]));
         let cors = Cors::default()
-            //.allowed_origin("194.58.90.123:8084")
-            .allowed_origin("194.58.90.123")
-            .allowed_origin("127.0.0.1")
+            .allowed_origin("194.58.90.123:8084")
+            .allowed_origin("194.58.90.123:8082")
+            .allowed_origin("127.0.0.1:6379")
             .allowed_methods(vec!["GET", "POST"])
             .max_age(3600);
 
