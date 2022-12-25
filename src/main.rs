0bf0e4a5b64612d5fa40e99ba9917136976a7d44
interesnij <ochkarik1983@mail.ru>
2022-12-25 08:21:38 +0300

    J

--- src/main.rs
+++ src/main.rs
@@ -61,10 +61,7 @@ async fn main() -> std::io::Result<()> {
         App::new()  
             .wrap(Logger::default())
             .wrap(Compress::default())
-            .wrap(SessionMiddleware::new(
-                CookieSessionStore::default(),
-                secret_key.clone(),
-            ))
+            .wrap(SessionMiddleware::new(CookieSessionStore::default(),secret_key.clone()))
             //.wrap(RedisSession::new("127.0.0.1:6379", &[0; 32]))
             .app_data(AppState {
                 server_id: SERVER_COUNTER.fetch_add(1, Ordering::SeqCst),
