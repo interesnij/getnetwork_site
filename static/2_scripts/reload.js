542a691a6cabe239c3aad61bbfa6726ac765ece9
interesnij <ochkarik1983@mail.ru>
2023-01-21 16:43:31 +0300

    J

--- static/2_scripts/reload.js
+++ static/2_scripts/reload.js
@@ -57,7 +57,7 @@ function check_first_load() {
             elem_ = document.createElement('span');
             elem_.innerHTML = ajax_link.responseText;
             document.body.innerHTML = elem_.innerHTML;
-            //loadScripts('/static/2_scripts/custom.js?ver1');
+            loadScripts('/static/2_scripts/custom.js?ver1');
             //loadScripts('/static/1_scripts/progressive-image.js');
             //get_or_create_cookie_user(); 
             //get_active_button();
